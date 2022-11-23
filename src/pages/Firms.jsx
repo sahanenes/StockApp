dd1076083447e0962fff8e1ed8d9c698ca8a12ad
sahanenes <sahanenesss@gmail.com>
2022-11-23 11:54:08 +0300

    usestockcalls added

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,17 +1,23 @@
 import axios from "axios";
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
+  const dispatch = useDispatch();
   const { token } = useSelector((state) => state.auth);
   const BASE_URL = "https://14276.fullstack.clarusway.com/";
   const getFirms = async () => {
+    const url = "firms";
+    dispatch(fetchStart());
     try {
       const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
         headers: { Authorization: `Token ${token}` },
       });
+      dispatch(getSuccess({ data, url }));
       console.log(data);
     } catch (error) {
+      dispatch(fetchFail());
       console.log(error);
     }
   };
