1d99dc9e38362ad219f5939f34c27d7d426dc21d
sahanenes <sahanenesss@gmail.com>
2022-11-23 13:43:23 +0300

    firm box added

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,17 +1,33 @@
+import { Box, Button, Grid, Typography } from "@mui/material";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 import useStockCalls from "../hooks/useStockCalls";
 // import axios from "axios";
 // import { useDispatch, useSelector } from "react-redux";
 // import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  const { getFirms, getSales } = useStockCalls();
+  const { firms } = useSelector((state) => state.stock);
+  const { getFirms } = useStockCalls();
   useEffect(() => {
     getFirms();
-    getSales();
   }, []);
 
-  return <div>Firms</div>;
+  return (
+    <Box>
+      <Typography variant="h4" color="error" mb={3}>
+        Firms
+      </Typography>
+      <Button variant="contained">New Firm</Button>
+      {firms?.firms.length > 0 && (
+        <Grid container>
+          {firms?.map((firm) => (
+            <Grid item></Grid>
+          ))}
+        </Grid>
+      )}
+    </Box>
+  );
 };
 
 export default Firms;
