0eb8fe4f07ff3478fe845a6f15b5e23d0d678bd0
sahanenes <sahanenesss@gmail.com>
2022-11-23 11:06:51 +0300

    get firms added

--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,4 +1,25 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+
 const Firms = () => {
+  const { token } = useSelector((state) => state.auth);
+  const BASE_URL = "https://14276.fullstack.clarusway.com/";
+  const getFirms = async () => {
+    try {
+      const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
+        headers: { Authorization: `Token ${token}` },
+      });
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFirms();
+  }, []);
+
   return <div>Firms</div>;
 };
 
