ee2ddbec8db4590a9d00d0d6e98bee6cfdd35fc0
sahanenes <sahanenesss@gmail.com>
2022-11-23 12:20:49 +0300

    get stock data added

---
+++ src/hooks/useStockCalls.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from "react-redux";
+import { axiosWithToken } from "../service/axiosInstance";
+import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+const useStockCalls = () => {
+  const dispatch = useDispatch();
+  const getStockData = async (url) => {
+    dispatch(fetchStart());
+    try {
+      const { data } = await axiosWithToken.get(`stock/${url}/`);
+      dispatch(getSuccess({ data, url }));
+    } catch (error) {
+      dispatch(fetchFail());
+      console.log(error);
+    }
+  };
+  const getFirms = () => getStockData("firms");
+  const getSales = () => getStockData("sales");
+  return { getFirms, getSales };
+};
+
+export default useStockCalls;
--- src/pages/Firms.jsx
+++ src/pages/Firms.jsx
@@ -1,29 +1,14 @@
-import axios from "axios";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
+import useStockCalls from "../hooks/useStockCalls";
+// import axios from "axios";
+// import { useDispatch, useSelector } from "react-redux";
+// import { fetchFail, fetchStart, getSuccess } from "../features/stockSlice";
 
 const Firms = () => {
-  const dispatch = useDispatch();
-  const { token } = useSelector((state) => state.auth);
-  const BASE_URL = "https://14276.fullstack.clarusway.com/";
-  const getFirms = async () => {
-    const url = "firms";
-    dispatch(fetchStart());
-    try {
-      const { data } = await axios.get(`${BASE_URL}stock/firms/`, {
-        headers: { Authorization: `Token ${token}` },
-      });
-      dispatch(getSuccess({ data, url }));
-      console.log(data);
-    } catch (error) {
-      dispatch(fetchFail());
-      console.log(error);
-    }
-  };
-
+  const { getFirms, getSales } = useStockCalls();
   useEffect(() => {
     getFirms();
+    getSales();
   }, []);
 
   return <div>Firms</div>;
