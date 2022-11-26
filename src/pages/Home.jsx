e1077d33b624c71bd8da818dd509e63a3a2c2d31
sahanenes <sahanenesss@gmail.com>
2022-11-27 02:32:53 +0300

    Home dashboard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,21 @@
+import { Box, Typography } from "@mui/material";
+import { useEffect } from "react";
+import useStockCalls from "../hooks/useStockCalls";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { getFirms, getSales } = useStockCalls();
+  useEffect(() => {
+    getFirms();
+    getSales();
+  }, []);
+
+  return (
+    <Box>
+      <Typography variant="h4" color="error" mb={3}>
+        Dashboard
+      </Typography>
+    </Box>
+  );
 };
 
 export default Home;
